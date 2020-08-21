5f8549f16913eff979c67de84367c79d950376fb
Illiana Gallardo <igallardo0716@gmail.com>
2020-08-21 20:02:09 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,9 +2,9 @@ def speak_to_grandma(phrase = "I LOVE YOU GRANDMA!")
   
   puts "HUH?! SPEAK UP, SONNY!"
   if phrase.upcase == phrase.upcase
-    puts "NO, NOT SINCE 1938!"
+    return "NO, NOT SINCE 1938!"
   else phrase == "I LOVE YOU GRANDMA"
-      puts "I LOVE YOU TOO PUMPKIN!"
+      return "I LOVE YOU TOO PUMPKIN!"
     end
   end
     
